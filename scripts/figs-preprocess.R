d78aad571bf99eb1654cfcf28dc59bb7ef5a46f5
harrig12 <cait.harrigan@mail.utoronto.ca>
2020-11-12 20:28:23 -0500

    split figs script, add 0.001 line, reduce waterfall plots

---
+++ scripts/figs-preprocess.R
@@ -0,0 +1,118 @@
+library(maftools)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(viridis)
+
+# taken from rmd
+
+# genes of interest
+goi <- c("TTN", "TP53", "KRAS", "BRCA1", "BRCA2", "ATM", "WNT1", "AKT1", "AKT2", "AKT3", "TSC1", "TSC2", "EGFR", 
+         "FGFR1", "ERBB2", "ERBB3", "ERBB4", "ROS1", "MET", "ALK", "FLT1", "PDGFRA", "FLT3", "FLT4", "RET", "FGFR2", 
+         "FGFR3", "DEPDC5", "NPRL2", "NPRL3", "MIOS", "SEH1L", "SEC13", "WDR24", "WDR59", "SLC38A9", "MTOR", "PTEN", "PIK3CA", "PIK3CB", "PIK3CG", "PIK3CD")
+goi <- factor(goi, levels = goi, ordered = T)
+
+# TMB calculations
+med_tmb <- function(m){ median(tmb(m)$total_perMB) }
+
+
+# MC3
+params = list(maf_dir = '../GDCdata/MC3',
+			  res_dir = '../results/MC3',
+			  names_tr = "../ref_files/cancer_types.txt"
+			  )
+
+# mafs
+maf_dir <- params$maf_dir
+maf_fns <- list.files(maf_dir, pattern = "*.maf$")
+mafs <- sapply(paste0(maf_dir, "/", maf_fns), read.maf)
+names(mafs) <- sapply(maf_fns, function(x){sub('\\.mutSig\\.maf$', '', x) })
+
+# cancer types
+cancer_names <- read.table(params$names_tr, header = T, sep = "\t")
+
+# mutsigCV results
+res_dir <- params$res_dir
+res_fns <- list.files(res_dir, pattern = "*.sig_genes.txt")
+mutsig_p <- data.frame(row.names = goi)
+
+# set up mutsig dataframe for genes of interest
+for (c in names(mafs)) {
+  ord <- match(c, substr(res_fns, 1, nchar(res_fns)-14))
+  mutsig_res <- read.table(paste0(res_dir, "/", res_fns[ord]), header=T)
+  poi <- mutsig_res[mutsig_res$gene %in% goi, c(1,14)]
+  mutsig_p[[c]] <- poi$p[match(goi, poi$gene)]
+}
+
+sampleSizes <- sapply(mafs, function(x){as.numeric(x@summary[ID %in% 'Samples', summary])})
+mutRates <- sapply(mafs, function(x){getGeneSummary(x)[match(goi, getGeneSummary(x)$Hugo_Symbol), MutatedSamples]})
+mutRates <- as.data.frame(apply(mutRates, denom = sampleSizes, FUN = function(x, denom){ x / denom * 100}, MAR = 1))
+colnames(mutRates) <- goi
+
+
+# renal subtyping
+
+renal_params = list(maf_dir = '../GDCdata/MC3-subtyped',
+			  		res_dir = '../results/MC3-subtyped',
+			  		names_tr = "../ref_files/cancer_subtypes.txt"
+			  		)
+
+maf_dir <- renal_params$maf_dir
+maf_fns <- list.files(maf_dir, pattern = "^TCGA-KIRC*")
+renal_mafs <- sapply(paste0(maf_dir, "/", maf_fns), read.maf)
+names(renal_mafs) <- sapply(maf_fns, function(x){sub('\\.mutSig\\.maf$', '', x) })
+
+# cancer types
+renal_names <- read.table(renal_params$names_tr, header = T, sep = "\t")
+
+# mutsigCV results
+res_dir <- renal_params$res_dir
+res_fns <- list.files(res_dir, pattern = "*.sig_genes.txt")
+mutsig_p <- data.frame(row.names = goi)
+
+# set up mutsig dataframe for genes of interest
+for (c in names(renal_mafs)) {
+  ord <- match(c, substr(res_fns, 1, nchar(res_fns)-14))
+  mutsig_res <- read.table(paste0(res_dir, "/", res_fns[ord]), header=T)
+  poi <- mutsig_res[mutsig_res$gene %in% goi, c(1,14)]
+  mutsig_p[[c]] <- poi$p[match(goi, poi$gene)]
+}
+
+renal_sampleSizes <- sapply(renal_mafs, function(x){as.numeric(x@summary[ID %in% 'Samples', summary])})
+renal_mutRates <- sapply(renal_mafs, function(x){getGeneSummary(x)[match(goi, getGeneSummary(x)$Hugo_Symbol), MutatedSamples]})
+renal_mutRates <- as.data.frame(apply(renal_mutRates, denom = renal_sampleSizes, FUN = function(x, denom){ x / denom * 100}, MAR = 1))
+colnames(renal_mutRates) <- goi
+
+#endometrial subtyping
+
+endo_params = list(maf_dir = '../GDCdata/MC3-subtyped',
+			  		res_dir = '../results/MC3-subtyped',
+			  		names_tr = "../ref_files/cancer_subtypes.txt"
+			  		)
+
+maf_dir <- endo_params$maf_dir
+maf_fns <- list.files(maf_dir, pattern = "^TCGA-UCEC*")
+endo_mafs <- sapply(paste0(maf_dir, "/", maf_fns), read.maf)
+names(endo_mafs) <- sapply(maf_fns, function(x){sub('\\.mutSig\\.maf$', '', x) })
+
+# cancer types
+endo_names <- read.table(endo_params$names_tr, header = T, sep = "\t")
+
+# mutsigCV results
+res_dir <- endo_params$res_dir
+res_fns <- list.files(res_dir, pattern = "*.sig_genes.txt")
+mutsig_p <- data.frame(row.names = goi)
+
+# set up mutsig dataframe for genes of interest
+for (c in names(endo_mafs)) {
+  ord <- match(c, substr(res_fns, 1, nchar(res_fns)-14))
+  mutsig_res <- read.table(paste0(res_dir, "/", res_fns[ord]), header=T)
+  poi <- mutsig_res[mutsig_res$gene %in% goi, c(1,14)]
+  mutsig_p[[c]] <- poi$p[match(goi, poi$gene)]
+}
+
+sampleSizes <- sapply(endo_mafs, function(x){as.numeric(x@summary[ID %in% 'Samples', summary])})
+mutRates <- sapply(endo_mafs, function(x){getGeneSummary(x)[match(goi, getGeneSummary(x)$Hugo_Symbol), MutatedSamples]})
+mutRates <- as.data.frame(apply(mutRates, denom = sampleSizes, FUN = function(x, denom){ x / denom * 100}, MAR = 1))
+colnames(mutRates) <- goi
+
--- scripts/figs.R
+++ scripts/figs.R
@@ -1,50 +1,4 @@
-library(maftools)
-library(dplyr)
-library(ggplot2)
-library(reshape2)
-library(viridis)
-
-# taken from rmd
-
-# genes of interest
-goi <- c("TTN", "TP53", "KRAS", "BRCA1", "BRCA2", "ATM", "WNT1", "AKT1", "AKT2", "AKT3", "TSC1", "TSC2", "EGFR", 
-         "FGFR1", "ERBB2", "ERBB3", "ERBB4", "ROS1", "MET", "ALK", "FLT1", "PDGFRA", "FLT3", "FLT4", "RET", "FGFR2", 
-         "FGFR3", "DEPDC5", "NPRL2", "NPRL3", "MIOS", "SEH1L", "SEC13", "WDR24", "WDR59", "SLC38A9", "MTOR", "PTEN", "PIK3CA", "PIK3CB", "PIK3CG", "PIK3CD")
-goi <- factor(goi, levels = goi, ordered = T)
-
-
-# MC3
-params = list(maf_dir = '../GDCdata/MC3',
-			  res_dir = '../results/MC3',
-			  names_tr = "../ref_files/cancer_types.txt"
-			  )
-
-# mafs
-maf_dir <- params$maf_dir
-maf_fns <- list.files(maf_dir, pattern = "*.maf$")
-mafs <- sapply(paste0(maf_dir, "/", maf_fns), read.maf)
-names(mafs) <- sapply(maf_fns, function(x){sub('\\.mutSig\\.maf$', '', x) })
-
-# cancer types
-cancer_names <- read.table(params$names_tr, header = T, sep = "\t")
-
-# mutsigCV results
-res_dir <- params$res_dir
-res_fns <- list.files(res_dir, pattern = "*.sig_genes.txt")
-mutsig_p <- data.frame(row.names = goi)
-
-# set up mutsig dataframe for genes of interest
-for (c in names(mafs)) {
-  ord <- match(c, substr(res_fns, 1, nchar(res_fns)-14))
-  mutsig_res <- read.table(paste0(res_dir, "/", res_fns[ord]), header=T)
-  poi <- mutsig_res[mutsig_res$gene %in% goi, c(1,14)]
-  mutsig_p[[c]] <- poi$p[match(goi, poi$gene)]
-}
-
-sampleSizes <- sapply(mafs, function(x){as.numeric(x@summary[ID %in% 'Samples', summary])})
-mutRates <- sapply(mafs, function(x){getGeneSummary(x)[match(goi, getGeneSummary(x)$Hugo_Symbol), MutatedSamples]})
-mutRates <- as.data.frame(apply(mutRates, denom = sampleSizes, FUN = function(x, denom){ x / denom * 100}, MAR = 1))
-colnames(mutRates) <- goi
+source('figs-preprocess.R')
 
 mutsig_p %>%
 	melt() %>%
@@ -52,20 +6,22 @@ mutsig_p %>%
 		   cancer = cancer_names$brief[match(variable, cancer_names$abbrev)],
 		   value = pmin(-log10(value), 5)) -> df
 
-png(file = '../plots_tmp/1c_mutsig.png')
+png(file = '../plots_tmp/mutsig_mtor.png')
 df %>% 
 	subset(Gene %in% c('PIK3CA', 'PTEN', 'MTOR')) %>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
 	ggplot(aes(fill=Gene, y=value, x=cancer)) + 
 		geom_bar(position="dodge", stat="identity") +
 		geom_hline(yintercept = -log10(0.05), size=0.2, linetype="dashed") +
-		annotate('text', x = 1, y = 1.75, label = 'p = 0.05', size = 2) +
+		annotate('text', x = 1, y = 1.7, label = 'p = 0.05', size = 2) +
+		geom_hline(yintercept = -log10(0.001), size=0.2, linetype="dashed") +
+		annotate('text', x = 1, y = 3.4, label = 'p = 0.001', size = 2) +
 		labs(title= 'Mutation frequency significance', x = '', y = expression(-log[10](p-value)) ) + 
 		scale_fill_viridis(discrete = T, direction = -1) +
 		coord_flip() + theme_classic()
 dev.off()
 
-png(file = '../plots_tmp/s1_mutsig_drivers.png')
+png(file = '../plots_tmp/mutsig_drivers.png')
 df %>% 
 	subset(Gene %in% c("TP53", "KRAS", "BRCA1", "BRCA2", "TTN")) %>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
@@ -78,7 +34,7 @@ df %>%
 		coord_flip() + theme_classic()
 dev.off()
 
-png(file = '../plots_tmp/s2_mutsig_rtks.png')
+png(file = '../plots_tmp/mutsig_rtks.png')
 df %>% 
 	subset(Gene %in% c("ERBB2", "EGFR")) %>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
@@ -91,7 +47,7 @@ df %>%
 		coord_flip() + theme_classic()
 dev.off()
 
-png(file = '../plots_tmp/s2_mutsig_gator1.png')
+png(file = '../plots_tmp/mutsig_gator1.png')
 df %>% 
 	subset(Gene %in% c("DEPDC5", "NPRL2", "NPRL3")) %>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
@@ -104,7 +60,7 @@ df %>%
 		coord_flip() + theme_classic()
 dev.off()
 
-png(file = '../plots_tmp/s2_mutsig_gator2.png')
+png(file = '../plots_tmp/mutsig_gator2.png')
 df %>% 
 	subset(Gene %in% c("MIOS", "SEH1L", "SEC13", "WDR24", "WDR59"))%>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
@@ -120,8 +76,9 @@ dev.off()
 # Waterfall plots
 
 for (m in c('TCGA-KIRC', 'TCGA-COAD', 'TCGA-UCEC', 'TCGA-MESO', 'TCGA-STAD')){
-	png(file = paste0('../plots_tmp/2c_waterfall_', m, '.png'))
-	PlotOncogenicPathways(mafs[[m]], pathways = "PI3K")
+	png(file = paste0('../plots_tmp/waterfall_', m, '.png'))
+	lil_m <- subsetMaf(mafs[[m]], genes = c("MTOR", "PTEN", "PIK3CA"))
+	PlotOncogenicPathways(lil_m, pathways = "PI3K", fontSize = 1)
 	title(main = cancer_names$brief[match(m, cancer_names$abbrev)])
 	dev.off()
 }
@@ -129,44 +86,13 @@ for (m in c('TCGA-KIRC', 'TCGA-COAD', 'TCGA-UCEC', 'TCGA-MESO', 'TCGA-STAD')){
 
 # Subtyped renal
 
-params = list(maf_dir = '../GDCdata/MC3-subtyped',
-			  res_dir = '../results/MC3-subtyped',
-			  names_tr = "../ref_files/cancer_subtypes.txt"
-			  )
-
-maf_dir <- params$maf_dir
-maf_fns <- list.files(maf_dir, pattern = "^TCGA-KIRC*")
-renal_mafs <- sapply(paste0(maf_dir, "/", maf_fns), read.maf)
-names(renal_mafs) <- sapply(maf_fns, function(x){sub('\\.mutSig\\.maf$', '', x) })
-
-# cancer types
-cancer_names <- read.table(params$names_tr, header = T, sep = "\t")
-
-# mutsigCV results
-res_dir <- params$res_dir
-res_fns <- list.files(res_dir, pattern = "*.sig_genes.txt")
-mutsig_p <- data.frame(row.names = goi)
-
-# set up mutsig dataframe for genes of interest
-for (c in names(renal_mafs)) {
-  ord <- match(c, substr(res_fns, 1, nchar(res_fns)-14))
-  mutsig_res <- read.table(paste0(res_dir, "/", res_fns[ord]), header=T)
-  poi <- mutsig_res[mutsig_res$gene %in% goi, c(1,14)]
-  mutsig_p[[c]] <- poi$p[match(goi, poi$gene)]
-}
-
-sampleSizes <- sapply(renal_mafs, function(x){as.numeric(x@summary[ID %in% 'Samples', summary])})
-mutRates <- sapply(renal_mafs, function(x){getGeneSummary(x)[match(goi, getGeneSummary(x)$Hugo_Symbol), MutatedSamples]})
-mutRates <- as.data.frame(apply(mutRates, denom = sampleSizes, FUN = function(x, denom){ x / denom * 100}, MAR = 1))
-colnames(mutRates) <- goi
-
-mutsig_p %>%
+renal_mutsig_p %>%
 	melt() %>%
 	mutate(Gene = rep(factor(goi, ordered = T), ncol(mutsig_p)),
-		   cancer = cancer_names$name[match(variable, cancer_names$abbrev)],
+		   cancer = renal_names$name[match(variable, renal_names$abbrev)],
 		   value = pmin(-log10(value), 5)) -> df
 
-png(file = '../plots_tmp/s3_mutsig_renal_subtypes.png')
+png(file = '../plots_tmp/mutsig_renal_subtypes.png')
 df %>% 
 	subset(Gene %in% c('PIK3CA', 'PTEN', 'MTOR')) %>%
 	mutate(cancer = ordered(cancer, levels = rev(unique(cancer[order(desc(value), desc(Gene))])))) %>%
@@ -174,10 +100,13 @@ df %>%
 		geom_bar(position="dodge", stat="identity") +
 		geom_hline(yintercept = -log10(0.05), size=0.2, linetype="dashed") +
 		annotate('text', x = 0.5, y = 1.75, label = 'p = 0.05', size = 3) +
+		geom_hline(yintercept = -log10(0.01), size=0.2, linetype="dashed") +
+		annotate('text', x = 0.5, y = 2, label = 'p = 0.01', size = 3) +
 		labs(title= 'Mutation frequency significance', x = '', y = expression(-log[10](p-value)) ) + 
 		scale_fill_viridis(discrete = T, direction = -1) +
 		coord_flip() + theme_classic()
 dev.off()
+
 
 
 
